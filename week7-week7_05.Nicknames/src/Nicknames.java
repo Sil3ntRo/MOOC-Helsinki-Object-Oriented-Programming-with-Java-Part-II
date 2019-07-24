b53c0ea7b640c11a18a8b40162e6e10c7a715357
Rohan Gayle <rgayle33614@gmail.com>
2019-07-23 22:06:45 -0400

    Completed Submission

--- week7-week7_05.Nicknames/src/Nicknames.java
+++ week7-week7_05.Nicknames/src/Nicknames.java
@@ -6,6 +6,14 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations requested in the assignment here!
+        HashMap<String, String> names = new HashMap<String, String>();
+        
+        names.put("matti", "mage");
+        names.put("mikael", "mixu");
+        names.put("arto", "arppa");
+        
+        System.out.println(names.get("mikael"));
+    
     }
 
 }
